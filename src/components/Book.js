230eaf4ad7195739b214e7f1a01d1221c37abcf6
ngodi <albertngodi@gmail.com>
2019-11-27 19:17:56 +0100

    Book component created

---
+++ src/components/Book.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Book from '../components/Book';
+
+
+class Book extends React.Component{
+ constructor(props){
+     super(props);
+
+ }
+ render(){
+     return(
+         <tr>
+             <td></td>
+             <td></td>
+             <td></td>
+         </tr>
+     )
+ }
+}
+
+export default Book;
--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Book from '../components/Book';
 
 
 const BooksList = () => {
