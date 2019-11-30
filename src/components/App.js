f7117e8bc95356bc79ad9477e2e25ecd689aa14d
ngodi <albertngodi@gmail.com>
2019-11-30 17:41:58 +0100

    bookform created

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import BooksForm from '../containers/BooksForm';
-import BooksList from '../containers/BooksList';
+// import BooksList from '../containers/BooksList';
 
 const App = () => {
     return (
         <div>
-            <BooksForm />
-            <BooksList />
+          <BooksForm />
         </div>
     )
 }
---
+++ src/containers/BooksForm.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './bookForm.css';
+
+
+const BooksForm = () => {
+    const categories = ["Action", "Biography", "History", "Horror", "Kids", "Learning", "Sci-Fi"];
+    const categoryList = categories.map(category => {
+       return  <option className='selectOption'>{category}</option>
+    });
+    return (
+        <div className='bookForm'>
+          <form>
+              <h2>Add book to store</h2>
+              <div class='formInput'>
+                  <label>Enter book title:</label><input type='text' /><br />
+              </div>
+              <div className='formSelect'>
+                  <label>Select category: </label><select>{categoryList}</select>
+              </div><br />
+              
+              <button type='submit'>submit</button>
+          </form>
+          
+        </div>
+    );
+};
+
+export default BooksForm;
