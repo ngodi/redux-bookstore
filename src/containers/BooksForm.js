3b952e654d52afc49f22ffe5e7edd324997fa969
ngodi <albertngodi@gmail.com>
2019-12-02 03:49:32 +0100

    handleChange implemented

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,10 +1,28 @@
+/* eslint-disable react/no-access-state-in-setstate */
+/* eslint-disable react/no-unused-state */
+/* eslint-disable react/state-in-constructor */
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable no-undef */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 import './bookForm.css';
 
 
-const BooksForm = () => {
+class BooksForm extends React.Component {
+  state = {
+    title: '',
+    category: '',
+  }
+
+handleChange = ({ target: { name, value } }) => {
+  this.setState({
+    ...this.state,
+    [name]: value,
+  });
+};
+
+render() {
   const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
   const categoryList = categories.map((category, i) => <option key={i} className="selectOption">{category}</option>);
   return (
@@ -14,7 +32,7 @@ const BooksForm = () => {
         <div className="formInput">
           <label htmlFor="title">
 Enter book title:
-            <input id="title" type="text" />
+            <input id="title" name="title" type="text" onChange={this.handleChange} />
           </label>
 
           <br />
@@ -22,7 +40,7 @@ Enter book title:
         <div className="formSelect">
           <label htmlFor="category">
 Select category:
-            <select id="category" name="category">{categoryList}</select>
+            <select id="category" name="category" onChange={this.handleChange}>{categoryList}</select>
           </label>
         </div>
         <br />
@@ -32,6 +50,7 @@ Select category:
 
     </div>
   );
-};
+}
+}
 
 export default BooksForm;
